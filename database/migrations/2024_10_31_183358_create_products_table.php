981a2123fea1114da39dfdb84c4aaee4a025492d
João Victor Pereira Rodrigues <jvpr123@outlook.com>
2024-11-03 16:30:00 -0300

    fix: adjust migration enum definition

--- database/migrations/2024_10_31_183358_create_products_table.php
+++ database/migrations/2024_10_31_183358_create_products_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('code');
-            $table->enum('status', ['published', 'draft', 'trashed'])->default('published');
+            $table->enum('status', ['published', 'draft', 'trash'])->default('published');
             $table->string('url')->nullable();
             $table->string('creator')->nullable();
             $table->string('product_name')->nullable();
