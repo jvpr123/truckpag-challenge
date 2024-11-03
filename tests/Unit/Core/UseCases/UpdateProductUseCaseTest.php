7a9871c1419f30c4cb529de4fa2fec8ed1e46963
João Victor Pereira Rodrigues <jvpr123@outlook.com>
2024-11-03 14:35:02 -0300

    test: add unit test to update product use-case

---
+++ tests/Unit/Core/UseCases/UpdateProductUseCaseTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Unit\Core\UseCases;
+
+use App\Core\Contracts\Repositories\ProductRepositoryInterface;
+use App\Core\Domain\Entities\Product;
+use App\Core\Domain\Enums\ProductStatus;
+use App\Core\Exceptions\ProductNotFoundException;
+use App\Core\UseCases\DTO\UpdateProductInputDTO;
+use App\Core\UseCases\UpdateProductUseCase;
+use App\Infrainstructure\Transformers\ProductTransformer;
+use App\Models\Product as ProductModel;
+use Carbon\Carbon;
+use Mockery;
+
+describe('UpdateProductUseCase -> execute()', function () {
+    beforeEach(function () {
+        Carbon::setTestNow(time());
+
+        $this->mockProductRepository = Mockery::mock(ProductRepositoryInterface::class);
+        $this->useCase = new UpdateProductUseCase($this->mockProductRepository);
+
+        $this->productModel = ProductModel::factory()->create();
+        $this->productEntity = (new ProductTransformer)->modelToEntity($this->productModel);
+
+        $this->inputDTO = new UpdateProductInputDTO(
+            id: $this->productModel->id,
+            code: $this->productModel->code,
+            status: 'draft',
+            creator: 'Product Creator',
+        );
+    });
+
+    it('should update a product successfully', function () {
+        $this->mockProductRepository
+            ->expects()
+            ->getProductByBarcode($this->productModel->code)
+            ->andReturn($this->productEntity)
+            ->once();
+
+        $this->mockProductRepository
+            ->expects()
+            ->updateProduct(Mockery::type(Product::class))
+            ->once();
+
+        $output = $this->useCase->execute($this->productModel->code, $this->inputDTO);
+        expect($output)->toBeInstanceOf(Product::class);
+        expect($output->getId())->toBe($this->productEntity->getId());
+        expect($output->getCode())->toBe($this->productEntity->getCode());
+        expect($output->getStatus())->toBe(ProductStatus::DRAFT);
+        expect($output->getCreator())->toBe('Product Creator');
+        expect($output->getUpdatedAt())->toBe(time());
+    });
+
+    it('should throw an exception if product not found', function () {
+        $this->mockProductRepository
+            ->expects()
+            ->getProductByBarcode($this->productModel->code)
+            ->andReturnNull()
+            ->once();
+
+        $this->useCase->execute($this->productModel->code, $this->inputDTO);
+    })->throws(ProductNotFoundException::class);
+
+    it('should throw an exception on updating product error', function () {
+        $this->mockProductRepository
+            ->expects()
+            ->getProductByBarcode($this->productModel->code)
+            ->andReturn($this->productEntity)
+            ->once();
+
+        $this->mockProductRepository
+            ->expects()
+            ->updateProduct(Mockery::type(Product::class))
+            ->andThrow(new \Exception('Error updating product.'))
+            ->once();
+
+        $this->useCase->execute($this->productModel->code, $this->inputDTO);
+    })->throws(\Exception::class, 'Error updating product.');
+});
--- tests/Unit/Infrastructure/Repositories/ProductRepositoryTest.php
+++ tests/Unit/Infrastructure/Repositories/ProductRepositoryTest.php
@@ -3,14 +3,16 @@
 namespace Tests\Unit\Infrastructure\Repositories;
 
 use App\Core\Domain\Entities\Product;
+use App\Core\Domain\Enums\ProductStatus;
 use App\Infrainstructure\Repositories\ProductRepository;
 use App\Infrainstructure\Transformers\ProductTransformer;
 use App\Models\Product as ProductModel;
+use Mockery;
 
 describe('ProductRepository -> getProductByBarcode()', function () {
     beforeEach(function () {
-        $this->mockProductTransformer = new ProductTransformer;
-        $this->productRepository = new ProductRepository($this->mockProductTransformer);
+        $this->productTransformer = new ProductTransformer;
+        $this->productRepository = new ProductRepository($this->productTransformer);
     });
 
     it('should return NULL if product not found', function () {
@@ -29,20 +31,42 @@ describe('ProductRepository -> getProductByBarcode()', function () {
 
 describe('ProductRepository -> update()', function () {
     beforeEach(function () {
-        $this->mockProductTransformer = new ProductTransformer;
+        $this->mockProductTransformer = Mockery::mock(ProductTransformer::class)->makePartial();
         $this->productRepository = new ProductRepository($this->mockProductTransformer);
+
+        $this->productModel = ProductModel::factory()->create();
+        $this->productEntity = (new ProductTransformer)->modelToEntity($this->productModel);
     });
 
-    it('should return NULL if product not found', function () {
-        $output = $this->productRepository->getProductByBarcode('00000');
-        expect($output)->toBeNull();
+    it('updates the product when it exists', function () {
+        $this->productEntity->setCode($updatedCode = '12345678');
+        $this->productEntity->setStatus($updatedStatus = ProductStatus::DRAFT);
+
+        $this->productModel->code = $updatedCode;
+        $this->productModel->status = $updatedStatus;
+
+        $this->mockProductTransformer
+            ->expects()
+            ->entityToModel($this->productEntity)
+            ->andReturn($this->productModel)
+            ->once();
+
+        $this->productRepository->updateProduct($this->productEntity);
+
+        $this->assertDatabaseHas('products', [
+            'id' => $this->productEntity->getId(),
+            'code' => $updatedCode,
+            'status' => $updatedStatus,
+        ]);
     });
 
-    it('should return a Product entity instance successfully', function () {
-        $product = ProductModel::factory()->create();
+    it('should throw an exception on product transformer error', function () {
+        $this->mockProductTransformer
+            ->expects()
+            ->entityToModel($this->productEntity)
+            ->andThrow(new \Exception('Error updating product.'))
+            ->once();
 
-        $output = $this->productRepository->getProductByBarcode($product->code);
-        expect($output)->toBeInstanceOf(Product::class);
-        expect($output->getId())->toBe($product->id);
-    });
+        $this->productRepository->updateProduct($this->productEntity);
+    })->throws(\Exception::class, 'Error updating product.');
 });
