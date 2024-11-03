33d03602cb94b09022ca33ba31b881e8c21244ae
João Victor Pereira Rodrigues <jvpr123@outlook.com>
2024-11-03 12:05:49 -0300

    feat: implement controller method for findProductByCode

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,7 +18,7 @@ class ProductController extends Controller
 
         return response()->json([
             'message' => 'Product found successfully.',
-            'product' => $product,
+            'product' => $product->toArray(),
         ]);
     }
 
