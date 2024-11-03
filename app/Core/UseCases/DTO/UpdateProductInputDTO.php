71ca5bdf6cb46e425db2e60952fd1aaaa4dd0b0c
João Victor Pereira Rodrigues <jvpr123@outlook.com>
2024-11-03 14:34:49 -0300

    feat: implement update product use-case

---
+++ app/Core/UseCases/DTO/UpdateProductInputDTO.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Core\UseCases\DTO;
+
+class UpdateProductInputDTO
+{
+    public function __construct(
+        public int $id,
+        public string $code,
+        public ?string $status = null,
+        public ?string $url = null,
+        public ?string $creator = null,
+        public ?string $productName = null,
+        public ?int $quantity = null,
+        public ?string $brands = null,
+        public ?string $categories = null,
+        public ?string $labels = null,
+        public ?string $cities = null,
+        public ?string $purchasePlaces = null,
+        public ?string $stores = null,
+        public ?string $ingredientsText = null,
+        public ?string $traces = null,
+        public ?string $servingSize = null,
+        public ?string $servingQuantity = null,
+        public ?string $nutriscoreScore = null,
+        public ?string $nutriscoreGrade = null,
+        public ?string $mainCategory = null,
+        public ?string $imageUrl = null,
+    ) {}
+}
---
+++ app/Core/UseCases/UpdateProductUseCase.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Core\UseCases;
+
+use App\Core\Contracts\Repositories\ProductRepositoryInterface;
+use App\Core\Domain\Entities\Product;
+use App\Core\Domain\Enums\ProductStatus;
+use App\Core\Exceptions\ProductNotFoundException;
+use App\Core\UseCases\DTO\UpdateProductInputDTO;
+
+class UpdateProductUseCase
+{
+    public function __construct(private ProductRepositoryInterface $productRepository) {}
+
+    public function execute(string $barcode, UpdateProductInputDTO $dto): Product
+    {
+        $product = $this->productRepository->getProductByBarcode($barcode);
+        if (!$product) {
+            throw new ProductNotFoundException();
+        }
+
+        $updatedProduct = $this->getProductToUpdate($product, $dto);
+        $this->productRepository->updateProduct($updatedProduct);
+
+        return $updatedProduct;
+    }
+
+    private function getProductToUpdate(Product $product, UpdateProductInputDTO $dto): Product
+    {
+        return new Product(
+            id: $product->getId(),
+            code: $dto->code ?: $product->getCode(),
+            status: $dto->status ? ProductStatus::from($dto->status) : $product->getStatus(),
+            url: $dto->url ?: $product->getUrl(),
+            creator: $dto->creator ?: $product->getCreator(),
+            productName: $dto->productName ?: $product->getProductName(),
+            quantity: $dto->quantity ?: $product->getQuantity(),
+            brands: $dto->brands ?: $product->getBrands(),
+            categories: $dto->categories ?: $product->getCategories(),
+            labels: $dto->labels ?: $product->getLabels(),
+            cities: $dto->cities ?: $product->getCities(),
+            purchasePlaces: $dto->purchasePlaces ?: $product->getPurchasePlaces(),
+            stores: $dto->stores ?: $product->getStores(),
+            ingredientsText: $dto->ingredientsText ?: $product->getIngredientsText(),
+            traces: $dto->traces ?: $product->getTraces(),
+            servingSize: $dto->servingSize ?: $product->getServingSize(),
+            serving_quantity: $dto->servingQuantity ?: $product->getServingQuantity(),
+            nutriscoreScore: $dto->nutriscoreScore ?: $product->getNutriscoreScore(),
+            nutriscoreGrade: $dto->nutriscoreGrade ?: $product->getNutriscoreGrade(),
+            mainCategory: $dto->mainCategory ?: $product->getMainCategory(),
+            imageUrl: $dto->imageUrl ?: $product->getImageUrl(),
+            importedAt: $product->getImportedAt(),
+            createdAt: $product->getCreatedAt(),
+            updatedAt: time()
+        );
+    }
+}
