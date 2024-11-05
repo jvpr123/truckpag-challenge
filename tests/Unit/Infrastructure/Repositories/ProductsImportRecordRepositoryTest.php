3f2cfad4718af9bb1c146e50f3d9bff13bdb99ce
João Victor Pereira Rodrigues <jvpr123@outlook.com>
2024-11-04 22:30:12 -0300

    test: add unit tests for register products file import repository method

--- tests/Unit/Infrastructure/Repositories/ProductsImportRecordRepositoryTest.php
+++ tests/Unit/Infrastructure/Repositories/ProductsImportRecordRepositoryTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Infrastructure\Repositories;
 
 use App\Infrainstructure\Repositories\ProductsImportRecordRepository;
 use App\Models\ProductsImportRecord;
+use Exception;
 
 describe('ProductsImportRecordRepository -> isFileAlreadyImported()', function () {
     beforeEach(fn() => $this->repository = new ProductsImportRecordRepository());
@@ -19,4 +20,29 @@ describe('ProductsImportRecordRepository -> isFileAlreadyImported()', function (
         $output = $this->repository->isFileAlreadyImported('filename');
         expect($output)->toBeFalse();
     });
+});
+
+describe('ProductsImportRecordRepository -> registerImportFileRecord()', function () {
+    beforeEach(fn() => $this->repository = new ProductsImportRecordRepository());
+
+    it('should register a products import file record successfully', function () {
+        $this->repository->registerImportFileRecord(
+            $filename = fake()->filePath(),
+            $success = fake()->boolean(),
+        );
+
+        $this->assertDatabaseHas('products_import_records', [
+            'imported_file' => $filename,
+            'success' => $success,
+        ]);
+    });
+
+    it('should throw an exception on saving record error', function () {
+        ProductsImportRecord::saving(fn() => throw new Exception('Error saving record.'));
+
+        $this->repository->registerImportFileRecord(
+            filename: fake()->filePath(),
+            success: fake()->boolean(),
+        );
+    })->throws(Exception::class, 'Error saving record.');
 });
